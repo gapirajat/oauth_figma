142e639f7ba1bb63b30bb68f0f6ffa168855357f
Rajat Shinde <86911396+gapirajat@users.noreply.github.com>
2024-06-06 06:53:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const crypto = require('crypto'); // Node.js built-in module for cryptographic functionality
 const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
